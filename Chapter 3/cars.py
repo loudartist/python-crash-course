d1bfeec0d83094aa7cc4f3a457b009ffe2d6843f
loudartist <github@urbasius.com>
2023-06-16 08:39:20 +0200

    more changes

---
+++ Chapter 3/cars.py
@@ -0,0 +1,17 @@
+cars =  ['bmw', 'audi', 'ford']
+# permanently changed order
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+cars =  ['bmw', 'audi', 'ford', 'test']
+# sorted
+print(cars)
+print(sorted(cars))
+print(cars)
+
+# length
+print(len(cars))
+
+print(cars[-1])
