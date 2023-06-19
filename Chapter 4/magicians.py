a54774dcd9c25d483452593beaed42a96a732fc8
loudartist <github@urbasius.com>
2023-06-19 21:03:33 +0200

    a new chapter started

---
+++ Chapter 4/magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'peter', 'john']
+
+for magician in magicians:
+    # print(magician)
+    print(f"{magician.title()}, that was a great trick!!")
+
+print("Thank's all!")
